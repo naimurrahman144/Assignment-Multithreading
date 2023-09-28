ee1f2778bae43a5ee1ffdb5e7429d75f1a3a3eb8
naimurrahman144 <139885741+naimurrahman144@users.noreply.github.com>
2023-09-28 18:12:46 +0600

    1st problem

---
+++ BankAccountThreadRunner.java
@@ -0,0 +1,21 @@
+package chapter_22;
+
+public class BankAccountThreadRunner {
+    public static void main(String[] args) {
+         BankAccount account = new BankAccount();
+         final double AMOUNT = 100;
+         final int REPETITIONS = 100;
+         final int THREADS = 100;
+
+         for (int i = 1; i <= THREADS; i++) {
+             DepositRunnable d = new DepositRunnable(account, AMOUNT, REPETITIONS);
+             WithdrawRunnable w = new WithdrawRunnable(account, AMOUNT, REPETITIONS);
+
+             Thread dt = new Thread(d);
+             Thread wt = new Thread(w);
+
+             dt.start();
+             wt.start();
+         }
+    }
+}
